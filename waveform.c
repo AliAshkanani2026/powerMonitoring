6274c6dee835f365aa7594ad0bb05c5144d726d7
ali_a <Ali2.Ashkanani@live.uwe.ac.uk>
2026-04-26 20:40:55 +0300

    voltage_peak_peak_calculator implemented successfully passed the phase to the function to avoid repeating too many codes tested : works well nexy step will be calculating dc offset for phases A , b , c

--- waveform.c
+++ waveform.c
@@ -32,3 +32,47 @@ double rms_calculator( struct WaveformSample * waveformdata , int data_counter ,
     double rms = sqrt(avg);
     return rms;
 }
+
+double voltage_peak_peak_calculator( struct WaveformSample * waveformdata , int data_counter , int phase ) {
+    int max = 0 ;
+    int min  = 0 ;
+    double Voltage_Value=0;
+    double vpp=0;
+    for ( int i =1 ; i <data_counter ; i++) {
+        if (phase == 1) //a
+        {
+            Voltage_Value = (waveformdata +i)->voltageA;
+            if (Voltage_Value > (waveformdata+max)->voltageA ) {
+                max=i;//update max value pos
+            }
+            if (Voltage_Value < (waveformdata+min)->voltageA ) {
+                min=i;
+            }
+             vpp = (waveformdata+max)->voltageA - (waveformdata+min)->voltageA;
+        }
+        if (phase == 2) //b
+        {
+            Voltage_Value = (waveformdata +i)->voltageB;
+            if (Voltage_Value > (waveformdata+max)->voltageB ) {
+                max=i;
+            }
+            if (Voltage_Value < (waveformdata+min)->voltageB ) {
+                min=i;
+            }
+            vpp = (waveformdata+max)->voltageB - (waveformdata+min)->voltageB;
+        }
+        if (phase == 3) //c
+        {
+            Voltage_Value = (waveformdata +i)->voltageC;
+            if (Voltage_Value > (waveformdata+max)->voltageC ) {
+                max=i;
+            }
+            if (Voltage_Value < (waveformdata+min)->voltageC ) {
+                min=i;
+            }
+            vpp = (waveformdata+max)->voltageC - (waveformdata+min)->voltageC;
+        }
+    }
+    return vpp;
+
+}
